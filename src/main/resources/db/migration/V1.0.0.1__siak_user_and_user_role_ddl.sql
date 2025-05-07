a124bc437606210c03dc53e55f77402747fed134
Muhammad Syaifullah Nurrahman <syaifullah.nurrahman@outlook.com>
2025-05-07 13:51:19 +0700

    update bcrypt seeder user

--- src/main/resources/db/migration/V1.0.0.1__siak_user_and_user_role_ddl.sql
+++ src/main/resources/db/migration/V1.0.0.1__siak_user_and_user_role_ddl.sql
@@ -1,3 +1,5 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
 -- SIAK USER TABLE --
 CREATE TABLE IF NOT EXISTS public.siak_user (
    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
@@ -36,11 +38,33 @@ CREATE INDEX IF NOT EXISTS idx_siak_user_role_role_id ON public.siak_user_role(s
 -- INSERT SIAK ROLE TABLE --
 INSERT INTO public.siak_role (id, nama, deskripsi, is_deleted, created_at, updated_at)
 VALUES
-    (gen_random_uuid(), 'KEUANGAN_UNIV',  'Role Univ',     FALSE, CURRENT_TIMESTAMP, NULL),
-    (gen_random_uuid(), 'KEUANGAN_FAK',   'Role Fakultas', FALSE, CURRENT_TIMESTAMP, NULL),
-    (gen_random_uuid(), 'KEUANGAN_PRODI', 'Role Prodi',    FALSE, CURRENT_TIMESTAMP, NULL),
-    (gen_random_uuid(), 'AKADEMIK_UNIV',  'Role Univ',     FALSE, CURRENT_TIMESTAMP, NULL),
-    (gen_random_uuid(), 'AKADEMIK_FAK',   'Role Fakultas', FALSE, CURRENT_TIMESTAMP, NULL),
-    (gen_random_uuid(), 'AKADEMIK_PRODI', 'Role Prodi',    FALSE, CURRENT_TIMESTAMP, NULL),
-    (gen_random_uuid(), 'MAHASISWA',      'Mahasiswa',     FALSE, CURRENT_TIMESTAMP, NULL),
-    (gen_random_uuid(), 'DOSEN',          'Dosen',         FALSE, CURRENT_TIMESTAMP, NULL);
+    ('00000000-0000-0000-0001-000000000001', 'KEUANGAN_UNIV',  'Role Univ',     FALSE, CURRENT_TIMESTAMP, NULL),
+    ('00000000-0000-0000-0002-000000000002', 'KEUANGAN_FAK',   'Role Fakultas', FALSE, CURRENT_TIMESTAMP, NULL),
+    ('00000000-0000-0000-0003-000000000003', 'KEUANGAN_PRODI', 'Role Prodi',    FALSE, CURRENT_TIMESTAMP, NULL),
+    ('00000000-0000-0000-0004-000000000004', 'AKADEMIK_UNIV',  'Role Univ',     FALSE, CURRENT_TIMESTAMP, NULL),
+    ('00000000-0000-0000-0005-000000000005', 'AKADEMIK_FAK',   'Role Fakultas', FALSE, CURRENT_TIMESTAMP, NULL),
+    ('00000000-0000-0000-0006-000000000006', 'AKADEMIK_PRODI', 'Role Prodi',    FALSE, CURRENT_TIMESTAMP, NULL),
+    ('00000000-0000-0000-0007-000000000007', 'MAHASISWA',      'Mahasiswa',     FALSE, CURRENT_TIMESTAMP, NULL),
+    ('00000000-0000-0000-0008-000000000008', 'DOSEN',          'Dosen',         FALSE, CURRENT_TIMESTAMP, NULL);
+
+INSERT INTO public.siak_user (id, username, email, password, is_deleted, created_at, updated_at)
+VALUES
+    ('00000000-0000-0001-0000-000000000001', 'adminakademikuniv', 'adminakademikuniv@mail.co', crypt('password123', gen_salt('bf', 6)), false, current_timestamp, null),
+    ('00000000-0000-0002-0000-000000000002', 'adminakademikfakultas', 'adminakademikfakultas@mail.co', crypt('password123', gen_salt('bf', 6)), false, current_timestamp, null),
+    ('00000000-0000-0003-0000-000000000003', 'adminakademikprodi', 'adminakademikprodi@mail.co', crypt('password123', gen_salt('bf', 6)), false, current_timestamp, null),
+    ('00000000-0000-0004-0000-000000000004', 'adminkeuanganuniv', 'adminkeuanganuniv@mail.co', crypt('password123', gen_salt('bf', 6)), false, current_timestamp, null),
+    ('00000000-0000-0005-0000-000000000005', 'adminkeuanganfakultas', 'adminkeuanganfakultas@mail.co', crypt('password123', gen_salt('bf', 6)), false, current_timestamp, null),
+    ('00000000-0000-0006-0000-000000000006', 'adminkeuanganprodi', 'adminkeuanganprodi@mail.co', crypt('password123', gen_salt('bf', 6)), false, current_timestamp, null),
+    ('00000000-0000-0007-0000-000000000007', 'mahasiswa', 'mahasiswa@mail.co', crypt('password123', gen_salt('bf', 6)), false, current_timestamp, null),
+    ('00000000-0000-0008-0000-000000000008', 'dosen', 'dosen@mail.co', crypt('password123', gen_salt('bf', 6)), false, current_timestamp, null);
+
+INSERT INTO public.siak_user_role (siak_user_id, siak_role_id, created_at, updated_at)
+VALUES
+    ('00000000-0000-0001-0000-000000000001', '00000000-0000-0000-0000-000000000004', current_timestamp, null),
+    ('00000000-0000-0002-0000-000000000002', '00000000-0000-0000-0000-000000000005', current_timestamp, null),
+    ('00000000-0000-0003-0000-000000000003', '00000000-0000-0000-0000-000000000006', current_timestamp, null),
+    ('00000000-0000-0004-0000-000000000004', '00000000-0000-0000-0000-000000000001', current_timestamp, null),
+    ('00000000-0000-0005-0000-000000000005', '00000000-0000-0000-0000-000000000002', current_timestamp, null),
+    ('00000000-0000-0006-0000-000000000006', '00000000-0000-0000-0000-000000000003', current_timestamp, null),
+    ('00000000-0000-0007-0000-000000000007', '00000000-0000-0000-0000-000000000007', current_timestamp, null),
+    ('00000000-0000-0008-0000-000000000008', '00000000-0000-0000-0000-000000000008', current_timestamp, null);
