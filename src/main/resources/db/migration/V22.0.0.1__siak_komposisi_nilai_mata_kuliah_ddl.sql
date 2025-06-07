8370a441f6cd57a5ca10002cb6d0783d0545265e
Muhamad Yusup <yusup.muhamad@team.cranium.id>
2025-06-08 00:27:30 +0700

    akhirnya kelar 😄

--- src/main/resources/db/migration/V22.0.0.1__siak_komposisi_nilai_mata_kuliah_ddl.sql
+++ src/main/resources/db/migration/V22.0.0.1__siak_komposisi_nilai_mata_kuliah_ddl.sql
@@ -4,5 +4,7 @@ CREATE TABLE IF NOT EXISTS public.siak_komposisi_nilai_mata_kuliah (
     siak_komposisi_nilai_id UUID NOT NULL,
     is_deleted bool NULL DEFAULT FALSE,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP
+    updated_at TIMESTAMP,
+    FOREIGN KEY (siak_mata_kuliah_id) REFERENCES siak_mata_kuliah(id),
+    FOREIGN KEY (siak_komposisi_nilai_id) REFERENCES siak_komposisi_nilai(id)
 );
